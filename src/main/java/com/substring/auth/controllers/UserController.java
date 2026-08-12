c89d79fd9bcf65a501fb4ba5fc3784db83174488
Sasha_Turchanskiy <turchanskiy777@gmail.com>
2026-08-12 17:53:43 +0300

    add user retrieval and management endpoints in UserController; introduce ApiResponse DTO for consistent responses

--- src/main/java/com/substring/auth/controllers/UserController.java
+++ src/main/java/com/substring/auth/controllers/UserController.java
@@ -1,14 +1,12 @@
 package com.substring.auth.controllers;
 
+import com.substring.auth.dtos.ApiResponse;
 import com.substring.auth.dtos.UserDto;
 import com.substring.auth.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,5 +19,31 @@ public class UserController {
     public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(userService.createUser(userDto));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<UserDto> getByEmail(@PathVariable String email) throws Exception {
+        return ResponseEntity.ok(userService.getUserByEmail(email));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Long userId) throws Exception {
+        return ResponseEntity.ok(userService.updateUser(userDto, userId.toString()));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getById(@PathVariable Long userId) throws Exception {
+        return ResponseEntity.ok(userService.getUserById(userId.toString()));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<UserDto>> getAll(){
+        return ResponseEntity.ok(userService.getAllUser());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) throws Exception {
+        userService.deleteUser(userId.toString());
+        return ResponseEntity.ok(new ApiResponse("User deleted successfully"));
     }
 }
